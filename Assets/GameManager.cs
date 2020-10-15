7f6be94e5a23942bc9a846f40ccce19732450734
jayanth <jayanthvoyager@gmail.com>
2020-10-15 11:46:41 +0530

    Init_Commit
    Final_commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Main_Jet;
+    public GameObject SubJet1;
+    public GameObject SubJet2;
+    public bool gameLost;
+    public bool gameWon;
+    public GameObject GameLost_Canvas;
+    public GameObject GameWon_Canvas;
+
+    public bool Main_Jet_Landed_Pause= false;
+    public bool SubJet1_Landed_Pause= false;
+    public bool SubJet2_Landed_Pause= false;
+
+    public GameObject UIMain_Jet;
+    public GameObject UI1Main_Jet;
+    public GameObject UISubJet1;
+    public GameObject UISubJet2;
+
+    public GameObject UIStopSubJet1;
+    public GameObject UIStopSubJet2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+       if( Main_Jet_Landed_Pause == false)
+        {
+            Main_Jet.GetComponent<Test>().GameOn();
+        }
+        if (SubJet1_Landed_Pause == false)
+        {
+            SubJet1.GetComponent<Test>().GameOn();
+        }
+        if (SubJet2_Landed_Pause == false)
+        {
+            SubJet2.GetComponent<Test>().GameOn();
+        }
+    }
+
+    public void subJet1_Landed_Pause()
+    {
+        SubJet1_Landed_Pause = true;
+        SubJet1.GetComponent<Test>().Fuel = 135;
+        SubJet1.GetComponent<Test>().IncreaseFuelSubJet2();
+    }
+
+    public void subJet2_Landed_Pause()
+    {
+        SubJet2_Landed_Pause = true;
+        SubJet2.GetComponent<Test>().Fuel = 135;
+        SubJet2.GetComponent<Test>().IncreaseFuelSubJet2();
+    }
+
+    public void subJet1_Landed_Go()
+    {
+        SubJet1_Landed_Pause = false;
+        
+    }
+
+    public void subJet2_Landed_Go()
+    {
+        SubJet2_Landed_Pause = false;
+       
+    }
+
+    public void GameLost()
+    {
+        if(gameLost==true)
+        GameLost_Canvas.SetActive(true);
+    }
+
+    public void Fuel_to_MainJet_From_SubJet1()
+    {
+       if(Main_Jet.GetComponent<Test>().Fuel<180)
+        {
+ Main_Jet.GetComponent<Test>().IncreseFuel_in_MainJet();
+        SubJet1.GetComponent<Test>().reduceFuelSubJet1();
+        }
+       
+    }
+
+    public void Fuel_to_MainJet_From_SubJet2()
+    {
+        if (Main_Jet.GetComponent<Test>().Fuel < 180)
+        {
+            Main_Jet.GetComponent<Test>().IncreseFuel_in_MainJet();
+            SubJet2.GetComponent<Test>().reduceFuelSubJet2();
+        }
+    }
+
+    public void IncreaseFueltoSubjet2()
+    {
+        if (SubJet2.GetComponent<Test>().Fuel < 180)
+        {
+            SubJet1.GetComponent<Test>().reduceFuelSubJet1();
+            SubJet2.GetComponent<Test>().IncreaseFuelSubJet2();
+        }
+    }
+
+    public void IncreaseFueltoSubjet1()
+    {
+        if (SubJet1.GetComponent<Test>().Fuel < 180)
+        {
+            SubJet2.GetComponent<Test>().reduceFuelSubJet2();
+            SubJet1.GetComponent<Test>().IncreaseFuelSubJet1();
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Main_Jet.GetComponent<Test>().FinalAngle == 360&& SubJet1.GetComponent<Test>().FinalAngle == 360 && SubJet2.GetComponent<Test>().FinalAngle == 360) 
+        {
+            if(gameLost == false)
+            {
+                GameWon_Canvas.SetActive(true);
+            }
+            else
+            {
+                GameLost();// other airplane crahed
+            }
+         
+        }
+        
+        if (SubJet1.GetComponent<Test>().FinalAngle != Main_Jet.GetComponent<Test>().FinalAngle)
+        {
+            if (Main_Jet.GetComponent<Test>().FinalAngle == 360 + (SubJet1.GetComponent<Test>().FinalAngle))
+            {
+                UIMain_Jet.SetActive(true);
+            }
+            else
+            {
+                UIMain_Jet.SetActive(false);
+            }
+
+        }
+        
+        if (Main_Jet.GetComponent<Test>().FinalAngle != SubJet2.GetComponent<Test>().FinalAngle)
+        {
+            if (Main_Jet.GetComponent<Test>().FinalAngle == 360 + (SubJet2.GetComponent<Test>().FinalAngle))
+            {
+                UI1Main_Jet.SetActive(true);
+            } 
+            else
+            {
+                UI1Main_Jet.SetActive(false);
+            }
+        }
+
+
+
+
+
+
+        if (SubJet1.GetComponent<Test>().FinalAngle != SubJet2.GetComponent<Test>().FinalAngle)
+        {
+            if (SubJet1.GetComponent<Test>().FinalAngle == 360 + (SubJet2.GetComponent<Test>().FinalAngle))
+            {
+
+                UISubJet1.SetActive(true);
+                UISubJet2.SetActive(true);
+            }
+            else
+            {
+                UISubJet1.SetActive(false);
+                UISubJet2.SetActive(false);
+            }
+        }
+        else
+        {
+            UISubJet1.SetActive(true);
+            UISubJet2.SetActive(true);
+        }
+
+            if (SubJet1.GetComponent<Test>().FinalAngle ==0) 
+        {
+            UIStopSubJet1.SetActive(true);
+
+        }
+        else
+        {
+            UIStopSubJet1.SetActive(false);
+        }
+
+        if (SubJet2.GetComponent<Test>().FinalAngle == 0)
+        {
+            UIStopSubJet2.SetActive(true);
+
+        }
+        else
+        {
+            UIStopSubJet2.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Test : MonoBehaviour
+{
+    public float Angle =45;
+    public Vector3 Right = new Vector3(0, 0,-1);
+    public Vector3 Left=new Vector3(0,0,1);
+    public Vector3 Direction;
+    public float InTime=2;
+    public bool Game_on = false;
+    public float Fuel = 180;
+    public Coroutine rotateObject;
+    public SpriteRenderer sprite;
+    public Slider slider;
+    public GameManager gameManager;
+
+    public float FinalAngle = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        Direction = Left;
+
+
+    }
+
+    public void Update()
+    {
+
+      
+    }
+
+    public void Rotate_Direction()
+    {
+        if (Direction == Left)
+        {
+            Direction = Right;
+            sprite.flipX = true;
+        }
+        else if ((Direction == Right))
+        {
+            Direction = Left;
+            sprite.flipX = false;
+        }
+    }
+
+    
+
+    public void reduceFuelSubJet1()
+    {
+        Fuel = Fuel-45;
+        slider.value = Fuel / 180;
+    }
+    public void IncreaseFuelSubJet1()
+    {
+        Fuel = Fuel + 45;
+        slider.value = Fuel / 180;
+    }
+
+        public void reduceFuelSubJet2()
+    {
+        Fuel = Fuel - 45;
+        slider.value = Fuel / 180;
+    }
+
+    public void IncreaseFuelSubJet2()
+    {
+        Fuel = Fuel + 45;
+        slider.value = Fuel / 180;
+    }
+
+        public void IncreseFuel_in_MainJet()
+    {
+        Fuel = Fuel + 45;
+        slider.value = Fuel / 180;
+    }
+   
+
+    public void GameOn()
+    {
+        Game_on = true;
+        if (Game_on == true)
+        {
+            rotateObject= StartCoroutine(RotateObject(Angle, Direction, InTime,Fuel));
+        }
+    }
+
+    IEnumerator RotateObject(float angle, Vector3 axis, float inTime,float Fuel)
+    {
+        if (Game_on == true)
+        {
+            // calculate rotation speed
+            float rotationSpeed = angle / inTime;
+
+            while (true)
+            {
+                // save starting rotation position
+                Quaternion startRotation = transform.rotation;
+
+                float deltaAngle = 0;
+
+                // rotate until reaching angle
+                while (deltaAngle < angle)
+                {
+                    float tempfuel = Fuel;
+                    tempfuel = tempfuel - deltaAngle;
+                    slider.value = tempfuel / 180;
+
+                    deltaAngle += rotationSpeed * Time.deltaTime;
+
+                    deltaAngle = Mathf.Min(deltaAngle, angle);
+
+                    transform.rotation = startRotation * Quaternion.AngleAxis(deltaAngle, axis);
+
+                    yield return null;
+                }
+
+                // delay here
+                yield return new WaitForSeconds(0.01f);
+                Fuel = Fuel - deltaAngle;
+
+                this.Fuel = this.Fuel - deltaAngle;
+                
+                FinalAngle = FinalAngle+ ((axis.z) *(deltaAngle));
+                Debug.Log("" + FinalAngle);
+                if (this.Fuel <= -10)
+                {
+                    gameManager.gameLost = true;
+                    gameManager.GameLost();
+                    //Engine Dead
+                }
+               // Debug.Log("" + Fuel);
+
+                Game_on = false;
+                StopCoroutine(rotateObject);
+            }
+        }
+    }
+}
